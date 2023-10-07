253b52b272154bb9f5c532df6ee516250f5abbc8
Hilal Morrar <hilalmorrar@gmail.com>
2023-10-07 04:22:35 -0500

    putting locks on everything.. slow but i think it's almost working

--- project1/frontend.py
+++ project1/frontend.py
@@ -12,8 +12,6 @@ baseAddr = "http://localhost:"
 baseServerPort = 9000
 
 rw = rwlock.RWLockFair()
-# rlock = rw.gen_rlock()
-# wlock = rw.gen_wlock()
 
 class SimpleThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
         pass
@@ -25,7 +23,6 @@ class FrontendRPCServer:
     ## servers that are responsible for inserting a new key-value
     ## pair or updating an existing one.
     def put(self, key, value):
-        print("----- PUT: KEY: ", key, "VALUE: ", value, "-----")
         with rw.gen_wlock():
             problem = None
             for id in kvsServers:
@@ -40,7 +37,6 @@ class FrontendRPCServer:
     ## servers that are responsible for getting the value
     ## associated with the given key.
     def get(self, key):
-        print("----- GET: ", key, "-----")
         with rw.gen_wlock():
             problem = None                
             # serverId = key % len(kvsServers)
@@ -56,15 +52,15 @@ class FrontendRPCServer:
     ## printKVPairs: This function routes requests to servers
     ## matched with the given serverIds.
     def printKVPairs(self, serverId):
-        if serverId not in kvsServers.keys():
-            return "ERR_NOEXIST"
-        return kvsServers[serverId].printKVPairs()
+        with rw.gen_wlock():
+            ret = kvsServers[serverId].printKVPairs()
+        return ret
 
     ## addServer: This function registers a new server with the
     ## serverId to the cluster membership.
     def addServer(self, serverId):
         with rw.gen_wlock():
-            with rw.gen_rlock():
+            for i in range(2):
                 if len(kvsServers) > 0:
                     firstID = random.choice(list(kvsServers.keys()))
                     kvsServers[serverId] = xmlrpc.client.ServerProxy(baseAddr + str(baseServerPort + serverId))
@@ -75,12 +71,8 @@ class FrontendRPCServer:
                         sp = kvp.split(":")
                         key = sp[0]
                         val = sp[1]
-                        # try:
+
                         kvsServers[serverId].put(key, val)
-                        # except:
-                        # kvsServers.pop(serverId)
-                            # break
-                    
                 else:
                     kvsServers[serverId] = xmlrpc.client.ServerProxy(baseAddr + str(baseServerPort + serverId))
         return "SUCCESS: FE ADD SERVER"
@@ -88,25 +80,27 @@ class FrontendRPCServer:
     ## listServer: This function prints out a list of servers that
     ## are currently active/alive inside the cluster.
     def listServer(self):
-        serverList = []
-        for serverId, rpcHandle in kvsServers.items():
-            try:
-                rpcHandle.heart()
-            except:
-                continue
-            serverList.append(serverId)
-        ret = ""
-        for s in serverList:
-            ret += str(s) + ","
+        with rw.gen_wlock():
+            ret = ""
+            serverList = []
+            for serverId, rpcHandle in kvsServers.items():
+                try:
+                    rpcHandle.heart()
+                except:
+                    continue
+                serverList.append(serverId)
+            for s in serverList:
+                ret += str(s) + ","
         return ret[:-1]
 
     ## shutdownServer: This function routes the shutdown request to
     ## a server matched with the specified serverId to let the corresponding
     ## server terminate normally.
     def shutdownServer(self, serverId):
-        result = kvsServers[serverId].shutdownServer()
-        kvsServers.pop(serverId)
-        return result
+        with rw.gen_wlock():
+            result = kvsServers[serverId].shutdownServer()
+            kvsServers.pop(serverId)
+        # return result
 
 server = SimpleThreadedXMLRPCServer(("localhost", 8001))
 socket.setdefaulttimeout(3)
